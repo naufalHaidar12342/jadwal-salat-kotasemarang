2a55815ae2db43453a0a8e37477ca136023272da
naufalHaidar12342 <111201912342@mhs.dinus.ac.id>
2023-08-26 22:09:12 +0700

    add dynamically generated opengraph (testing soon)

---
+++ app/opengraph-image.tsx
@@ -0,0 +1,40 @@
+import { data } from "autoprefixer";
+import { ImageResponse } from "next/server";
+
+export const runtime = "edge";
+
+export const alt = "Jadwal Sholat Semarang";
+export const size = {
+	width: 1200,
+	height: 630,
+};
+export const contentType = "image/png";
+
+export default async function Image() {
+	// font
+	const interLight = await fetch(
+		new URL("./fonts/Inter-Light.ttf", import.meta.url)
+	).then((res) => res.arrayBuffer());
+
+	return new ImageResponse(
+		(
+			<div
+				style={{
+					fontSize: 48,
+					backgroundColor: "#656564",
+					background: "#5682e5",
+					width: "100%",
+					height: "100%",
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+				}}
+			>
+				Waktu salat subuh hingga isya untuk umat Islam di Kota Semarang 📿
+			</div>
+		),
+		{
+			...size,
+		}
+	);
+}
