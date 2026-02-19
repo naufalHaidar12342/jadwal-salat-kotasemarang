324002540aa31abbf31816be3115a1c7aad6cbe9
naufalHaidar12342 <naufalhaidar12342@gmail.com>
2026-02-20 03:40:24 +0700

    fix (vercel build error): re-importing Footer using auto-complete feature in VSCode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import "./globals.css";
 import { Open_Sans } from "next/font/google";
-import Footer from "@/app/components/Footer";
 import NavigationBar from "@/app/components/NavigationBar";
 import TitleBar from "./components/TitleBar";
 import { Metadata } from "next";
 import MobileNavigationBar from "./components/MobileNavigationBar";
+import Footer from "./components/Footer";
 
 const openSans = Open_Sans({ subsets: ["latin"] });
 
